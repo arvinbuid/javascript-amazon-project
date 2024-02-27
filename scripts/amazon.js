8c91e17044a2d9f5eb2122a0ef297de1159f9a52
ArvinBuid22 <arvinbuid01@gmail.com>
2024-02-27 13:53:13 +0800

    Fix price formatting, radio selectors and changed a little bit of styling on radio selector margin.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart, addToCart} from "../data/cart.js";
 import {products} from "../data/products.js";
+import {formatCurrency} from "./utilities/money.js";
 
 let innerHTML = "";
 
@@ -22,7 +23,7 @@ products.forEach((product) => {
         <div class="product-rating-count link-primary">${product.rating.count}</div>
         </div>
 
-        <div class="product-price">$${product.priceCents / 100}</div>
+        <div class="product-price">$${formatCurrency(product.priceCents)}</div>
 
         <div class="product-quantity-container">
         <select>
