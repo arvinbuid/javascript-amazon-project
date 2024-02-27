4427782338eceb095df743b9bfbf286be98363ae
ArvinBuid22 <arvinbuid01@gmail.com>
2024-02-27 14:53:14 +0800

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -33,4 +33,18 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+}
+
+// Remove item from the cart
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  // Add each cartItem into the new array except if it has the productId
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
