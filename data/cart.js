905e58ac50abf1ff2290cb4d5f0061bf7d31e574
ArvinBuid22 <arvinbuid01@gmail.com>
2024-02-27 18:36:48 +0800

    Save cart into localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,29 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-  {
-    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    quantity: 3,
-  },
-];
+// loading the cart from localStorage
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+// if the cart is empty, assign default value to cart
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+    {
+      productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      quantity: 3,
+    },
+  ];
+}
+
+// Function to save the cart into localStorage
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   // Variable to store the same items in the cart
@@ -33,6 +45,8 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+
+  saveToStorage();
 }
 
 // Remove item from the cart
@@ -47,4 +61,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
